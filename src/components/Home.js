b0a2d499254b0fac172ba3f50ec399b7cddb52d8
Narendra N Shetty <mailnarendra15@gmail.com>
2016-12-11 10:49:36 +0100

    added html point

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,46 @@ import Drawer from 'react-toolbox/lib/drawer';
 import classNames from 'classnames';
 import { hashHistory } from 'react-router';
 import MapGL from 'react-map-gl';
+import HTMLOverlay from 'react-map-gl/dist/overlays/html.react.js';
 import Dimensions from 'react-dimensions';
+import Immutable from 'immutable';
+import ViewportMercator from 'viewport-mercator-project';
+
+function round(x, n) {
+  const tenN = Math.pow(10, n);
+  return Math.round(x * tenN) / tenN;
+}
 
 export const Home = React.createClass({
 
   getInitialState() {
     return {
       'active': false,
-      'latitude': 37.7577,
-      'longitude': -122.4376
+      'viewport': {
+        'width': this.props.containerWidth,
+        'height': this.props.containerHeight,
+        'mapStyle': 'mapbox://styles/mapbox/basic-v9',
+        'mapboxApiAccessToken': 'pk.eyJ1IjoibmFyZW5kcmFzaGV0dHkiLCJhIjoiY2l3am9veHJ2MDAwbDJ0cjI1NTkyM3llNSJ9.l2l38Z5jAyCO0_aOE-ABlA',
+        'latitude': 37.7577,
+        'longitude': -122.4376,
+        'zoom': 12.011557070552028,
+        'startDragLngLat': null,
+        'isDragging': null
+      },
+      'locations': [-122.4376, 37.7577]
     };
   },
 
   componentDidMount() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((pos) => {
-        this.setState({
+        const viewport = Object.assign({}, this.state.viewport, {
           'latitude': pos.coords.latitude,
           'longitude': pos.coords.longitude
+        });
+        this.setState({
+          viewport,
+          'locations': [pos.coords.longitude, pos.coords.latitude]
         });
       });
     }
@@ -124,24 +146,52 @@ export const Home = React.createClass({
   },
 
   render() {
-
+    // let HTMLViewport = this.state.viewport;
+    
     return (
       <div className="fullHeight">
         <div className='fullWidth fullHeight map'>
           <MapGL
-            width={this.props.containerWidth}
-            height={this.props.containerHeight}
-            latitude={this.state.latitude}
-            longitude={this.state.longitude}
-            zoom={12.011557070552028}
-            startDragLngLat={null}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            isDragging={true}
-            mapboxApiAccessToken="pk.eyJ1IjoibmFyZW5kcmFzaGV0dHkiLCJhIjoiY2l3am9veHJ2MDAwbDJ0cjI1NTkyM3llNSJ9.l2l38Z5jAyCO0_aOE-ABlA"
-            onChangeViewport={viewport => {
-              const {latitude, longitude, zoom} = viewport;
+            {...this.state.viewport}
+            onChangeViewport={(newViewport) => {
+              const viewport = Object.assign({}, this.state.viewport, newViewport);
+              this.setState({viewport});
             }}
-          />
+          >
+            <HTMLOverlay
+              width={this.state.viewport.width}
+              height={this.state.viewport.height}
+              isDragging={false}
+              redraw={(config) => {
+                const project = config.project(config.isDragging);
+                return (
+                  <div style={{
+                    'backgroundColor': '#000',
+                    'width': 10,
+                    'height': 10,
+                    'position': 'absolute',
+                    'borderRadius': 10,
+                    'top': project[1],
+                    'left': project[0]
+                  }}>
+                  </div>
+                );
+              }}
+              project={() => {
+                let viewport = this.state.viewport;
+                const mercator = ViewportMercator({
+                  longitude: viewport.longitude,
+                  latitude: viewport.latitude,
+                  zoom: viewport.zoom,
+                  width: viewport.width,
+                  height: viewport.height
+                });
+
+                const pixel = mercator.project(this.state.locations);
+                return [round(pixel[0], 1), round(pixel[1], 1)];
+              }}
+            />
+          </MapGL>
         </div>
         {this.renderSearchResult()}
         {this.renderSearchbox()}
