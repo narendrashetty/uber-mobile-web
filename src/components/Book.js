38236364d1785adeda6fe29ca5cc4b62c8d9fee2
Narendra N Shetty <mailnarendra15@gmail.com>
2016-12-18 10:48:08 +0100

    added data attrs to book route

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,30 +4,46 @@ export const Book = React.createClass({
 
   render() {
     return (
-      <div className="book">
+      <div className="book" data-rideid="0">
         <div className="book__header">
-          Economy
+          <div className="book__header__name" data-id="0">
+            Economy
+          </div>
+          <div className="book__header__name" data-id="1">
+            Extra Seats
+          </div>
         </div>
 
 
         <div className="book__body">
           <div className="rideOptions">
-            <div className="ride">
-              <div className="ride__image"></div>
-              <p className="ride__name">uberPOOL</p>
-              <p className="ride__price">106.75</p>
+
+            <div className="rideGroup fullHeight fullWidth" data-id="0">
+              <div className="ride">
+                <div className="ride__image"></div>
+                <p className="ride__name">uberPOOL</p>
+                <p className="ride__price">106.75</p>
+              </div>
+
+              <div className="ride">
+                <div className="ride__image"></div>
+                <p className="ride__name">uberGO</p>
+                <p className="ride__price">106.75</p>
+              </div>
+
+              <div className="ride">
+                <div className="ride__image"></div>
+                <p className="ride__name">uberX</p>
+                <p className="ride__price">106.75</p>
+              </div>
             </div>
 
-            <div className="ride">
-              <div className="ride__image"></div>
-              <p className="ride__name">uberGO</p>
-              <p className="ride__price">106.75</p>
-            </div>
-
-            <div className="ride">
-              <div className="ride__image"></div>
-              <p className="ride__name">uberX</p>
-              <p className="ride__price">106.75</p>
+            <div className="rideGroup fullHeight fullWidth" data-id="1">
+              <div className="ride">
+                <div className="ride__image"></div>
+                <p className="ride__name">uberX</p>
+                <p className="ride__price">106.75</p>
+              </div>
             </div>
           </div>
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -164,7 +164,6 @@ export const Map = React.createClass({
         }}
       >
         {this.renderOverlay()}
-
       </MapGL>
     );
   },
